8f46f3e4804963bdf4fabf1578a89d49a721977e
lbourlon <leonbourlon@hotmail.com>
2021-11-29 10:16:56 +0100

    Tidying up the repo

--- manage.py
+++ manage.py
@@ -3,24 +3,24 @@
 from sys import argv
 from os import system
 
-usr_local = ["dell_mode", "cpu_set", "sudowoodo.png"]
-rest = ["pacman.conf", "xorg.conf"]
+conf = ["pacman.conf", "xorg.conf"]
+extra = ["dell_mode", "cpu_set", "sudowoodo.png"]
 
 # Checks the diff between git_files and system_files
 def diff():
-    for e in rest:
+    for e in conf:
         diff = 0
         if (e == "xorg.conf"):
-            diff = system(f"diff /etc/X11/{e} ./{e} --color")
+            diff = system(f"diff /etc/X11/{e} ./conf/{e} --color")
         else :
-            diff = system(f"diff /etc/{e} ./{e} --color")
+            diff = system(f"diff /etc/{e} ./conf/{e} --color")
         
 
         if (diff != 0):
             print(f"^ Diffs dans {e} ^")
 
-    for e in usr_local :
-        diff = system(f"diff /usr/local/bin/{e} ./{e} --color")
+    for e in extra:
+        diff = system(f"diff /usr/local/bin/{e} ./extra/{e} --color")
 
         if (diff != 0):
             print(f"^ Diffs dans {e} ^")
@@ -28,13 +28,13 @@ def diff():
 # Fetches system files to update the git repo.
 def fetch():
     for e in usr_local:
-        system(f"cp /usr/local/bin/{e} ./{e}")
+        system(f"cp /usr/local/bin/{e} ./extra/{e}")
 
     for e in rest:
         if(e == "xorg.conf"):
-            system(f"cp /etc/X11/{e} ./{e}")
+            system(f"cp /etc/X11/{e} ./conf/{e}")
         else :
-            system(f"cp /etc/{e} ./{e}")
+            system(f"cp /etc/{e} ./conf/{e}")
 
 
 # Pushes the files in the repo into the system.
