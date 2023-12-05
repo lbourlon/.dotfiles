91560b85b16eed2be11f72629ab94d309b2ec491
Léon Bourlon <leonbourlon@hotmail.com>
2023-12-05 09:37:20 +0100

    nvim version

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-sudo apt update
-sudo apt install neovim spice-vdagent kitty
+#sudo apt update
+#sudo apt install neovim spice-vdagent kitty
 
-wget https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz
-sha256sum nvim-linux64.tar.gz| grep 6c083017304213c3a3efde8d332a52231b8df8206d35146942097c303ebf93d5
-tar xzvf nvim-linux64.tar.gz
-sudo cp ./nvim-linux64/bin/nvim /bin/nvim
+wget https://github.com/neovim/neovim/releases/download/v0.9.4/nvim-linux64.tar.gz
+sha256sum nvim-linux64.tar.gz| grep dbf4eae83647ca5c3ce1cd86939542a7b6ae49cd78884f3b4236f4f248e5d447
+#tar xzvf nvim-linux64.tar.gz
+#sudo cp ./nvim-linux64/bin/nvim /bin/nvim
 
 # Prompt for these and replace
 # git config --global user.name ""
